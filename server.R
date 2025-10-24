e29d9d479254c91af8074b328f3966d683cf0a29
Sven Gato Redsun <svengato@svengato.com>
2025-10-24 14:03:36 -0600

    Automatically update landscape for any input change

--- server.R
+++ server.R
@@ -4,11 +4,12 @@ source("fractal-landscape.R", local = TRUE)
 
 # Define the server logic
 shinyServer(function(input, output) {
-  output$plotFL <- renderRglwidget({
-    mx.alt <- fractalLandscape(altitudeScale = input$sldAlt, fractalDimension = input$sldDim,
-      seed = input$spnSeed)
-    plotFractalLandscape(mx.alt, displayWater = input$chkWater)
-    rglwidget()
+  observe({
+    output$plotFL <- renderRglwidget({
+      mx.alt <- fractalLandscape(altitudeScale = input$sldAlt, fractalDimension = input$sldDim, seed = input$spnSeed)
+      plotFractalLandscape(mx.alt, displayWater = input$chkWater)
+      rglwidget()
+    })
   })
 })
 
